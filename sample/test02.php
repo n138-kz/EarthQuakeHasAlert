c379a338eb6750897fea6375fdbbd157cbc2facd
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-05-26 23:09:14 +0900

    sample\test02.php

---
+++ sample/test02.php
@@ -0,0 +1,63 @@
+<?php
+date_default_timezone_set('Asia/Tokyo');
+
+$url = 'https://www.data.jma.go.jp/developer/xml/feed/extra.xml';
+$xml = simplexml_load_file( $url );
+$json = json_encode( $xml );
+$array = json_decode( $json, TRUE );
+
+echo $array['title'] . PHP_EOL;
+echo $array['updated'] . PHP_EOL;
+echo $array['id'] . PHP_EOL;
+
+try {
+	$pdo = new PDO( 'sqlite::memory:' );
+	$sql  = '';
+	$sql .= '';
+	$sql .= 'CREATE TABLE tmp01 ( id text, title text, updated text, author_name text, content text );';
+	$pdo->query($sql);
+
+	$sql  = '';
+	$sql .= '';
+	$sql .= 'SELECT * FROM tmp01;';
+	$stm = $pdo->query($sql);
+	$res = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+} catch ( \Exception $e ) {
+	var_dump( $e->getMessage() );
+}
+
+foreach( $array['entry'] as $key=>$val ) {
+        if( $val['title'] != '震源・震度に関する情報' ){
+                continue;
+        }
+
+	try {
+		$sql  = '';
+		$sql .= '';
+		$sql .= 'INSERT INTO tmp01 ( id, title, updated, author_name, content ) VALUES (?, ?, ?, ?, ?);';
+		$stm = $pdo->prepare($sql);
+		$stm -> execute([
+			$val['id'],
+			$val['title'],
+			$val['updated'],
+			$val['author']['name'],
+			$val['content'],
+		]);
+	
+		$sql  = '';
+		$sql .= '';
+		$sql .= 'SELECT * FROM tmp01;';
+		$stm = $pdo->query($sql);
+		$res = $stm->fetchAll(PDO::FETCH_ASSOC);
+		foreach( $res as $key => $val ) {
+			echo date( 'Y/m/d H:i:s T', strtotime( $val['updated'] ) );
+			echo chr(9);
+			echo mb_convert_kana( $val['content'], 'as' );
+			echo PHP_EOL;
+		}
+
+	} catch ( \Exception $e ) {
+		var_dump( $e->getMessage() );
+	}
+}
