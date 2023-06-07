e7c732aaa1f8bf568d274fdeb6fe45fa94de6f4d
Yuu Komiya <yu.komiya@n138.jp>
2023-06-07 10:38:58 +0900

    各地の震度の項目が不定形だったので定形に変更

--- sample2/xmlfeed_eqvol.php
+++ sample2/xmlfeed_eqvol.php
@@ -29,6 +29,18 @@ foreach($data['entry'] as $key => $val){
 	, TRUE);
 }
 
+/* 各地の震度の項目が不定形だったので定形に変更 */
+foreach($data['entry'] as $key => $val){
+	if( isset( $val['detail']['Body']['Intensity']['Observation']['Pref']['Area'] ) ) {
+		/* $data['entry'][$key] --> $val */
+		$data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'][0] = $val['detail']['Body']['Intensity']['Observation']['Pref']['Area'];
+		unset( $data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area']['City'] );
+		unset( $data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area']['Code'] );
+		unset( $data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area']['Name'] );
+		unset( $data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area']['MaxInt'] );
+	}
+}
+
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 header('Content-Type: application/json');
