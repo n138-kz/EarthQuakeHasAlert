7f2eea9945629c50a8c38be1d042efebbb6f68b1
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-06-09 14:04:25 +0900

    クラス定義を外部ファイル化（php-internal.php）

--- sample2/xmlfeed_eqvol.php
+++ sample2/xmlfeed_eqvol.php
@@ -32,45 +32,7 @@ function loadSystemSecret($secret_keyfile = 'secret.txt'){
 	$secret_keyfile = json_decode($secret_keyfile, TRUE);
 	return $secret_keyfile;
 }
-class internalDB {
-	private $database;
-	function __construct($database) {
-		$this->database=[
-			'path'=>'',
-			'mode'=>0,
-		];
-		if ( !is_readable($database) ) {
-			error_log('Faild open database file(mode=r): ' . $database);
-		} else {
-			$this->database['mode'] = $this->database['mode']|1; /* 001 */
-		}
-		if ( !is_writable($database) ) {
-			error_log('Faild open database file(mode=w): ' . $database);
-		} else {
-			$this->database['mode'] = $this->database['mode']|2; /* 010 */
-		}
-		$this->database['path']=$database;
-	}
-	function insert($query=[]){
-		if ( $this->database['mode']&3 !== 3 ) {
-			error_log('error: no permission to database in write.');
-			return FALSE;
-		}
-		$data=$this->select();
-		$data[]=$query;
-		$data=json_encode($data, JSON_NUMERIC_CHECK|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_INVALID_UTF8_IGNORE);
-		return file_put_contents($this->database['path'], $data);
-	}
-	function select($query=[]){
-		if ( $this->database['mode']&1 !== 1 ) {
-			error_log('error: no permission to database.');
-			return FALSE;
-		}
-		$data=file_get_contents($this->database['path']);
-		$data=json_decode($data, TRUE);
-		return $data;
-	}
-}
+require_once './php-internal.php';
 $database['useraccesslog']=new internalDB(dirname(__FILE__).'/'.'database_useraccess.db');
 if ( mb_strtolower($_SERVER['REQUEST_METHOD']) != 'get' ) {
 	http_response_code(405); 
