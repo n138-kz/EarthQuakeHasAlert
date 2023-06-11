de64e065c1919d423cf9e88b0a8f9d8ab7ea8bbe
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-06-11 18:40:20 +0900

    Create manage.php

---
+++ manage.php
@@ -0,0 +1,42 @@
+<?php session_start();
+require_once './vendor/autoload.php';
+require_once './php-internal.php';
+
+define('CLIENT_ID', $_SESSION['user']['google']['client_id']);
+define('CLIENT_TOKEN', $_SESSION['user']['google']['client_token']);
+
+$client = new Google_Client(['client_id' => CLIENT_ID]); 
+$payload = $client->verifyIdToken(CLIENT_TOKEN);
+if (!$payload) {
+	http_response_code(401);
+    die('Unauthorized');
+}
+
+?><!DOCTYPE html><html lang="ja">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>xmlfeed_eqvol.php</title>
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js?render=6LfCHdcUAAAAAOwkHsW_7W7MfoOrvoIw9CXdLRBA"></script>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+    <script src="./lib/grecaptcha.js" id="grecaptchajs"></script>
+    <script src="./lib/isset.js"></script>
+    <script>
+        window.addEventListener('load', function() {
+            console.log('page is fully loaded');
+            getFeedXML_eqvol();
+        });
+        window.addEventListener('DOMContentLoaded', function() {
+            init_pageloaded_eqvol();
+        });
+        setInterval(() => {
+            getFeedXML_eqvol();
+        }, 1000*10);
+    </script>
+    <style>
+    </style>
+</head>
+<body>
+</body>
+</html>
