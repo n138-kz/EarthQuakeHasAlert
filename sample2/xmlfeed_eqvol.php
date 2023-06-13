0ade3c6c51c0610ee6c66424d58a00c351568aba
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-06-13 20:33:26 +0900

    可読性

--- sample2/xmlfeed_eqvol.php
+++ sample2/xmlfeed_eqvol.php
@@ -213,13 +213,15 @@ if (!$data) {
 		/* 各地の震度の項目が不定形だったので定形に変更 */
 		error_log( '['.$_SERVER['REMOTE_ADDR'].']'.json_encode( [ 'isset->('.__LINE__.')', isset( $data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'] ) ] ) );
 		if( isset( $data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'] ) ) {
-			$data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'][] = $val['Body']['Intensity']['Observation']['Pref']['Area'];
+			$data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'][]=
+			$data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'];
 		}
 
 		foreach($val['Body']['Intensity']['Observation']['Pref']['Area'] as $key2 => $val2){
 			error_log( '['.$_SERVER['REMOTE_ADDR'].']'.json_encode( [ 'isset->('.__LINE__.')', isset( $data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'][$key2]['City']['Name'] ) ] ) );
 			if( isset( $val['Body']['Intensity']['Observation']['Pref']['Area'][$key2]['City']['Name'] ) ) {
-				$data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'][$key2]['City'][] = $val['Body']['Intensity']['Observation']['Pref']['Area'][$key2]['City'];
+				$data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'][$key2]['City'][]=
+				$data['entry'][$key]['detail']['Body']['Intensity']['Observation']['Pref']['Area'][$key2]['City'];
 			}
 
 		}
